cca9c8b7ac1ffe9c1f1f2b7a8290f5abd4c8ee0f
MATHEUS HENRY DOS SANTOS <25204051@CFP127ALUNO.LOCAL>
2026-03-10 16:56:06 -0300

    Calculadora de desconto finalizada

---
+++ 02-calculadora-desconto/app.js
@@ -0,0 +1,38 @@
+function removerClasses(){
+    const resultado = document.getElementById("resultado")
+
+    resultado.classList.remove("desconto-medio")
+    resultado.classList.remove("desconto-alto")
+    resultado.classList.remove("desconto-baixo")
+}
+
+function calcularDesconto(){
+    const valorDaCompra = document.getElementById("valor")
+    const desconto      = document.getElementById("desconto")
+
+    let economizou = valorDaCompra.value * (desconto.value / 100)
+    let diferenca  = valorDaCompra.value - economizou
+
+    exibirResultado(economizou, desconto.value, diferenca)
+}
+
+function exibirResultado(economizou, descont, diferenca){
+    let difer       = diferenca
+    let economizado = economizou
+    let desconto    = descont
+    const exibir    = document.getElementById("resultado")
+    
+    if(desconto <= 5){
+        exibir.textContent = `Você economizou R$${economizado.toFixed(2)} e o preço final é R$${difer.toFixed(2)}`
+        removerClasses()
+        exibir.classList.add("desconto-baixo")
+    }else if(desconto > 5 && desconto <= 10){
+        exibir.textContent = `Você economizou R$${economizado.toFixed(2)} e o preço final é R$${difer.toFixed(2)}`
+        removerClasses()
+        exibir.classList.add("desconto-medio")
+    }else{
+        exibir.textContent = `Você economizou R$${economizado.toFixed(2)} e o preço final é R$${difer.toFixed(2)}`
+        removerClasses()
+        exibir.classList.add("desconto-alto")
+    }
+}
